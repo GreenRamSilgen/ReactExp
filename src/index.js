4bddcb6461818e6857967a3e18e119ba7d0df60d
GreenRamSilgen <ps3iswow@gmail.com>
2020-08-06 14:34:46 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Carousel from 'react-elastic-carousel';
+ 
+class App extends React.Component {
+  state = {
+    items: [
+      {id: 1, title: 'https://i.pinimg.com/236x/f7/47/83/f74783ce63362f6fe5334b11384db577.jpg'},
+      {id: 2, title: 'https://i.pinimg.com/236x/11/2e/3b/112e3b9716252fe63dd89893ba57d4ed.jpg'},
+      {id: 3, title: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3f/BTS%2C_Wings.jpg/220px-BTS%2C_Wings.jpg'},
+      {id: 4, title: 'https://64.media.tumblr.com/06f03431eaf42f7a065fd91d4656c81c/tumblr_p9ard1w4Sa1vxmh1zo4_250.jpg'},
+      {id: 5, title: 'https://i.pinimg.com/236x/eb/cc/d5/ebccd590f63848a7a5aef38ceb64517d.jpg'}
+    ]
+  }
+ 
+  render () {
+    const { items } = this.state;
+    return (
+        <div className='carouselContainer'>
+      <Carousel>
+        {items.map(item => <div key={item.id}><img src={item.title} alt="yo"/></div>)}
+      </Carousel>
+      </div>
+    )
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App/>, document.getElementById('root'));
